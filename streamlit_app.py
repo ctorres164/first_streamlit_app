0d9b643bb4ae87d4de41681c1f9880e7c8cb2aa4
CTorres <107567842+ctorres164@users.noreply.github.com>
2022-06-17 09:28:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,28 +10,5 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]               
-
-streamlit.dataframe(fruits_to_show)
-
-#new section to display fruityvice api response
-stremalit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like informat about?', 'Kiwi')
-stremlit.write('The user entered', fruit_choice)
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-#take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output in the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-                      
                       
